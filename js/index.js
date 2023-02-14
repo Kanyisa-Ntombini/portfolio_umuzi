50ee9016a2ca596681995b242c45c3a5c74e6ccb
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2023-02-14 22:09:54 +0200

    Finished responsive design for header

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const hamburgerIcon = document.getElementById('hamburgerIcon');
+const sideNavWidth = document.getElementById('sideNavWidth');
+const happy = document.getElementById('happy');
+
+const openNav = () => {
+  sideNavWidth.style.width = '100%';
+  hamburgerIcon.style.display = 'none';
+};
+
+const closeNav = () => {
+  sideNavWidth.style.width = 0;
+  hamburgerIcon.style.display = 'block';
+};
