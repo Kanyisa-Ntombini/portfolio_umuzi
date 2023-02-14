def13b1add9f4bf0e793f9e342608e2f06c37f72
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2023-02-14 22:02:05 +0200

    Rearranged folder structure

--- resources/js/index.js
+++
@@ -1,7 +0,0 @@
-const openNav = () => {
-  document.getElementById("sideNavWidth").style.width = "14rem";
-}
-
-const closeNav = () => {
-  document.getElementById("sideNavWidth").style.width = "0rem";
-}
